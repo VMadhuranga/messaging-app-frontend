57893af318ed558b1d5f287dc3a489b21a335c32
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-16 12:07:59 +0530

    Add signUpAction function to /signup route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,33 @@
+import { redirect } from "react-router-dom";
 import App from "./components/app/App";
 import SignUpPage from "./components/sign-up-page/SignUpPage";
+import signUpAction from "./actions/sign-up-action";
 
+const baseUrl = import.meta.env.VITE_BACKEND_URL;
 const routes = [
   {
     path: "/",
     element: <App />,
     children: [
       {
-        path: "login",
+        path: "/login",
         element: <p>Login page</p>,
       },
       {
-        path: "signup",
+        path: "/signup",
         element: <SignUpPage />,
+        action: async ({ request }) => {
+          const formData = Object.fromEntries(
+            (await request.formData()).entries(),
+          );
+          const errorData = await signUpAction(baseUrl, formData);
+
+          if (errorData) {
+            return errorData;
+          }
+
+          return redirect("/login");
+        },
       },
     ],
   },
