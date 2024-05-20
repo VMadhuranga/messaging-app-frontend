97654903d7c3aa72f71eb3e2c29dcb1b3c4d6adc
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-20 10:45:45 +0530

    Create loginAction function

---
+++ src/actions/login-action.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default async function loginAction(baseUrl, formData) {
+  try {
+    const response = await axios.post(`${baseUrl}/login`, formData, {
+      withCredentials: true,
+    });
+
+    const { userId } = response.data;
+
+    return { userId };
+  } catch (error) {
+    const { data: errorData } = error.response.data;
+
+    return { errorData };
+  }
+}
