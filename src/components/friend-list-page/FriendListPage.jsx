8d3d0a476f6b0a75b5acdbf6756c86f64b9fc75b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-05-30 13:30:29 +0530

    Remove tests

---
+++ src/components/friend-list-page/FriendListPage.jsx
@@ -0,0 +1,31 @@
+import { Link, useLoaderData, useParams } from "react-router-dom";
+
+const FriendListPage = () => {
+  const friends = useLoaderData();
+  const { user_id } = useParams();
+
+  return (
+    <section data-testid="friend_list_page">
+      <h2>Friends</h2>
+      {friends.length > 0 ? (
+        <ul data-testid="friend_list">
+          {friends.map(({ friend }) => (
+            <li key={friend._id}>
+              <Link
+                to={`${friend._id}`}
+              >{`${friend.firstName} ${friend.lastName}`}</Link>
+              <span>{friend.userName}</span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>There are no friends yet</p>
+      )}
+      <p>
+        <Link to={`/${user_id}/people`}>Browse people</Link>
+      </p>
+    </section>
+  );
+};
+
+export default FriendListPage;
--- tests/utils/drop-test-db.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default async function dropTestDb() {
-  await axios.get("http://localhost:3000/drop", {
-    withCredentials: true,
-  });
-}
