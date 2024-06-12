73b6e3bee100b589e0bea02924614be3c1d408b2
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-12 13:25:34 +0530

    Setup add friend form

--- src/components/browse-people-page/BrowsePeoplePage.jsx
+++ src/components/browse-people-page/BrowsePeoplePage.jsx
@@ -1,26 +1,28 @@
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 
 const BrowsePeoplePage = () => {
   const people = useLoaderData();
-  console.log(people);
+  const fetcher = useFetcher();
 
   return (
     <section>
       <h2>Browse people</h2>
       {/* render search bar */}
-      {people.length && (
+      {people.length > 0 ? (
         <ul>
           {people.map((person) => (
             <li key={person._id}>
               {person.firstName} {person.lastName}
               <span>{person.userName}</span>
-              <form>
-                <input type="hidden" name="" />
+              <fetcher.Form method="post">
+                <input type="hidden" name="friend_id" value={person._id} />
                 <button>Add to friends</button>
-              </form>
+              </fetcher.Form>
             </li>
           ))}
         </ul>
+      ) : (
+        <p>There are no people who are not on your friend list.</p>
       )}
     </section>
   );
