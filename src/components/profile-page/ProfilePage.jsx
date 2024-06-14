c82a6663b6a471f4bd5445b24247a67589209c2c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-14 14:01:29 +0530

    Create initial ProfilePage component

---
+++ src/components/profile-page/ProfilePage.jsx
@@ -0,0 +1,19 @@
+const ProfilePage = () => {
+  return (
+    <section>
+      <h2>User&apos;s name</h2>
+      <div>
+        <a>Edit profile</a>
+      </div>
+      <div>
+        <a>Delete profile</a>
+      </div>
+      <form>
+        <input type="hidden" />
+        <button type="submit">Logout</button>
+      </form>
+    </section>
+  );
+};
+
+export default ProfilePage;
