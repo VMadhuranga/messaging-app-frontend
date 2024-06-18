4d9755aae4e29ac0836dbc091b0e6935f28079cb
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-18 13:53:01 +0530

    Implement Delete profile link and deleteProfile function

--- src/components/profile-page/ProfilePage.jsx
+++ src/components/profile-page/ProfilePage.jsx
@@ -1,6 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ProfilePage = () => {
+  const params = useParams();
+
+  function deleteProfile(e) {
+    if (!confirm("Do you really want to delete the profile")) {
+      e.preventDefault();
+    }
+  }
+
   return (
     <section>
       <h2>User&apos;s name</h2>
@@ -8,7 +16,9 @@ const ProfilePage = () => {
         <a>Edit profile</a>
       </div>
       <div>
-        <a>Delete profile</a>
+        <Link to={`/${params.user_id}/profile/delete`} onClick={deleteProfile}>
+          Delete profile
+        </Link>
       </div>
       <div>
         <Link to={"/logout"}>Logout</Link>
