7bb44f60a3c03c8eed9f33cccf16c17949267a9e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-06-19 13:59:22 +0530

    Create initial EditProfilePage component

---
+++ src/components/edit-profile-page/EditProfilePage.jsx
@@ -0,0 +1,71 @@
+const EditProfilePage = () => {
+  return (
+    <section>
+      <h2>Edit profile</h2>
+      <form>
+        <div>
+          <label htmlFor="first_name">First name</label>
+          <input type="text" id="first_name" name="first_name" required />
+        </div>
+        <div>
+          <button type="submit">Update first name</button>
+        </div>
+      </form>
+      <hr />
+      <form>
+        <div>
+          <label htmlFor="last_name">Last name</label>
+          <input type="text" id="last_name" name="last_name" required />
+        </div>
+        <div>
+          <button type="submit">Update last name</button>
+        </div>
+      </form>
+      <hr />
+      <form>
+        <div>
+          <label htmlFor="username">User name</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <button type="submit">Update user name</button>
+        </div>
+      </form>
+      <hr />
+      <form>
+        <div>
+          <label htmlFor="old_password">Old password</label>
+          <input
+            type="password"
+            id="old_password"
+            name="old_password"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="new_password">New password</label>
+          <input
+            type="password"
+            id="new_password"
+            name="new_password"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm_new_password">Confirm new password</label>
+          <input
+            type="password"
+            id="confirm_new_password"
+            name="confirm_new_password"
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Update password</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default EditProfilePage;
