6b140c5a62c5c35573ec27910cfc39d70ecd4de0
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-07-05 14:26:23 +0530

    Add ChatPage component styles

--- src/components/chat-page/ChatPage.jsx
+++ src/components/chat-page/ChatPage.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import {
   Form,
+  Link,
   useFetcher,
   useLoaderData,
   useLocation,
   useParams,
 } from "react-router-dom";
+import sendSvg from "../../assets/icons/send.svg";
+import deleteSvg from "../../assets/icons/delete.svg";
+import goBackSvg from "../../assets/icons/go-back.svg";
+import styles from "./ChatPage.module.css";
 
 const ChatPage = () => {
   const messages = useLoaderData();
@@ -15,39 +20,65 @@ const ChatPage = () => {
   const [message, setMessage] = useState("");
 
   return (
-    <section>
+    <section className={`defaultSection ${styles.chatPage}`}>
       <div>
+        <Link to={`/${params.user_id}/friends`} className="goBackBtn">
+          <img src={goBackSvg} alt="Go back" title="Go back" />
+        </Link>
         <h2>{location.state.friendName}</h2>
-        <Form method="delete">
+        <Form method="delete" className={styles.deleteFriendForm}>
           <input type="hidden" value={params.friend_id} />
-          <button type="submit">Delete friend</button>
+          <button type="submit">
+            <img
+              src={deleteSvg}
+              alt="Delete the friend"
+              title="Delete the friend"
+            />
+          </button>
         </Form>
       </div>
-      {messages.length > 0 ? (
-        <ul>
-          {messages
-            .sort((a, b) => new Date(a.date) - new Date(b.date))
-            .map((message) => (
-              <li key={message._id}>{message.content}</li>
-            ))}
-        </ul>
-      ) : (
-        <p>There are no messages yet</p>
-      )}
+      <ul>
+        {messages.length > 0 ? (
+          <>
+            {messages
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .map((message) => (
+                <li
+                  key={message._id}
+                  className={
+                    message.senderID === params.user_id
+                      ? `${styles.userMsg}`
+                      : `${styles.friendMsg}`
+                  }
+                >
+                  {message.content}
+                </li>
+              ))}
+          </>
+        ) : (
+          <p>There are no messages yet</p>
+        )}
+      </ul>
       <fetcher.Form
         method="post"
         onSubmit={() => {
           setMessage("");
         }}
+        className={`defaultForm ${styles.sendMessageForm}`}
       >
         <textarea
           name="message"
+          required
+          placeholder="Type your message here"
+          maxLength="65"
           value={message}
           onChange={(e) => {
             setMessage(e.target.value);
           }}
         ></textarea>
-        <button type="submit">Send message</button>
+        <button type="submit">
+          <img src={sendSvg} alt="Send the message" title="Send the message" />
+        </button>
       </fetcher.Form>
     </section>
   );
