24188c951e4ea1e4f937bd7c58497c033444da9f
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-08-17 09:15:27 +0530

    Update fall back element

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import "./main.css";
+import { LoadingIndicator } from "./components/loading-indicator/LoadingIndicator";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider
-      router={routes}
-      fallbackElement={<p className="fallback">Loading...</p>}
-    />
+    <RouterProvider router={routes} fallbackElement={<LoadingIndicator />} />
   </React.StrictMode>,
 );
